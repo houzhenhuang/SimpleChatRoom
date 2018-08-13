a194d324f211de8ce76f301df631a32e9281071a
huanghouzhen <1165071241@qq.com>
2018-08-13 18:07:18 +0800

    first commit project

---
+++ SimpleChatRoom/ServerHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SimpleChatRoom
+{
+    public class ServerHub : Hub
+    {
+        public void AddToRoom(string roomName,string userName)
+        {
+            Groups.Add(Context.ConnectionId, roomName);
+
+            Clients.Group(roomName, new string[0]).addUserIn(roomName,userName);
+        }
+        public void Send(string roomName, string userName,string msg)
+        {
+            Clients.Group(roomName, new string[0]).sendMessage(userName,msg);
+        }
+    }
+}
---
+++ SimpleChatRoom/obj/Debug/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimpleChatRoom.MvcApplication" Language="C#" %>
